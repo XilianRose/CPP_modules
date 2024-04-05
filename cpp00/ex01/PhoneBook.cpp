01a9fbfafe2e42a70e72e3769911085c8af35da2
mstegema <mstegema@student.codam.nl>
2024-04-05 15:36:59 +0200

    start on ex01

---
+++ cpp00/ex01/PhoneBook.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   PhoneBook.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/04/05 12:35:53 by mstegema      #+#    #+#                 */
+/*   Updated: 2024/04/05 15:34:52 by mstegema      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void){
+	std::cout << "Constructor called" << std::endl;
+	return;
+}
+
+PhoneBook::~PhoneBook(void){
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+void	PhoneBook::add(void){
+
+	return;
+}
+
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   PhoneBook.hpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/04/05 12:22:44 by mstegema      #+#    #+#                 */
+/*   Updated: 2024/04/05 15:11:07 by mstegema      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class PhoneBook {
+	public:
+	PhoneBook(void);
+	~PhoneBook(void);
+
+	void	add(void);
+};
