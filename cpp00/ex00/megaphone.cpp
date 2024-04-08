29317b08055a163632bbf42360713c4932ff0415
mstegema <mstegema@student.codam.nl>
2024-04-08 15:16:30 +0200

    change method to only output once

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/04 14:50:45 by mstegema      #+#    #+#                 */
-/*   Updated: 2024/04/05 11:08:16 by mstegema      ########   odam.nl         */
+/*   Updated: 2024/04/08 15:09:00 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,21 @@
 
 int	main(int argc, char** argv)
 {
-	int j = 0;
+	std::string	input;
 
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
-	{
-		for (int i = 1; i < argc; i++)
+	{	
+		for (int i = 1; argv[i] != nullptr; i++)
 		{
-			while (argv[i][j] != '\0')
-			{
-				argv[i][j] = std::toupper(argv[i][j]);
-				std::cout << argv[i][j];
-				j++;
-			}
-			j = 0;
+			input = input + argv[i];
 		}
-		std::cout << std::endl;
+		for (int i = 0; input[i] != '\0'; i++)
+		{
+			input[i] = std::toupper(input[i]);
+		}
+		std::cout << input << std::endl;
 	}
 	return 0;
 }
