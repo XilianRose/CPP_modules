257ece96c062894f736dc8cb7758b612818004be
Michelle <m.stegeman@me.com>
2024-05-02 19:03:14 +0200

    split add function into 2 to take out reusable code

--- cpp00/ex01/src/PhoneBook.cpp
+++ cpp00/ex01/src/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/05 12:35:53 by mstegema      #+#    #+#                 */
-/*   Updated: 2024/05/02 17:54:04 by mstegema      ########   odam.nl         */
+/*   Updated: 2024/05/02 19:01:46 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,46 +23,40 @@ PhoneBook::~PhoneBook(void){
 	return;
 }
 
-void	PhoneBook::add(void){
-	std::string	input;
+void PhoneBook::_gatherInfo(std::string& field, const std::string& prompt) {
+	while (1) {
+		std::cout << prompt;
+		if (std::getline(std::cin, field) && field != "") {
+			break;
+		}
+		std::cout << "Field can't be left empty." << std::endl;
+	}
+}
 
-	while(1)
-	{
-		input = "";
-		std::cout << "First name	: ";
-		if (std::getline(std::cin, input) && input != "")
-		{
-			this->_contacts[this->_index % 8].FirstName = input;
-			break;
-		}
-		std::cout << "Field can't be left empty." << std::endl;
+void PhoneBook::add() {
+	Contact new_contact;
+
+	new_contact = this->_contacts[this->_index % 8];
+	if (this->_index > 7){
+		std::cout << "Warning! Contact " << new_contact.firstName << " " << new_contact.lastName << " will be overwritten." << std::endl;
 	}
-	while(1)
-	{
-		input = "";
-		std::cout << "Last name	: ";
-		if (std::getline(std::cin, input) && input != "")
-		{
-			this->_contacts[this->_index % 8].LastName = input;
-			break;
-		}
-		std::cout << "Field can't be left empty." << std::endl;
-	}
-	std::cout << "FirstName: " << this->_contacts[this->_index % 8].FirstName << std::endl;
-	std::cout << "LastName: " << this->_contacts[this->_index % 8].LastName << std::endl;
+	_gatherInfo(new_contact.firstName, "First name	: ");
+	_gatherInfo(new_contact.lastName, "Last name	: ");
+	_gatherInfo(new_contact.nickname, "Nickname	: ");
+	_gatherInfo(new_contact.phoneNumber, "Phone number	: ");
+	_gatherInfo(new_contact.darkestSecret, "Darkest secret	: ");
+
+	// this->_contacts[this->_index % 8] = new_contact;
 	this->_index++;
-	return;
 }
 
 void	PhoneBook::search(void){
 	std::string	input;
 
-	while(1)
-	{
+	while(1){
 		input = "";
 		std::cout << "Enter the index: ";
-		if (std::getline(std::cin, input) && input != "")
-		{
+		if (std::getline(std::cin, input) && input != ""){
 			break;
 		}
 		std::cout << "Field can't be left empty." << std::endl;
