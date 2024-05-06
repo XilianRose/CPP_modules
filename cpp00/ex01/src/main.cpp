cd577d4ba581aa3e7d3123a4231040b8522538ee
mstegema <mstegema@student.codam.nl>
2024-05-06 12:39:19 +0200

    add minor menu changes

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/05 12:33:49 by mstegema      #+#    #+#                 */
-/*   Updated: 2024/05/02 19:00:13 by mstegema      ########   odam.nl         */
+/*   Updated: 2024/05/06 12:26:15 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ int	main(void){
 	PhoneBook	phoneBook;
 	std::string	input;
 
-	while(1)
-	{
+	while(1){
 		input = "";
-		std::cout << "What would you like to do?" << std::endl;
-		std::cout << "ADD: save a new contact,	SEARCH: display a specific contact,	EXIT: exit ByteBook" << std::endl;
+		std::cout << std::endl << "What would you like to do?" << std::endl;
+		std::cout << "ADD	: save a new contact" << std::endl;
+		std::cout << "SEARCH	: display a specific contact" << std::endl;
+		std::cout << "EXIT	: exit ByteBook" << std::endl;
 		std::cout << "> ";
 		std::getline(std::cin, input);
 		if (input == "ADD")
@@ -31,8 +32,7 @@ int	main(void){
 			phoneBook.search();
 		else if (input == "EXIT")
 			return(0);
-		else
-		{
+		else{
 			std::cout << "Please enter a valid option." << std::endl;
 			continue;
 		}
