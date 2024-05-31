7d2b1c3c83c2febfb51058455dd8515b6e5a7f2b
mstegema <mstegema@student.codam.nl>
2024-05-31 12:10:27 +0200

    finish complain function using pointers to memberfunctions

--- cpp01/ex05/src/Harl.cpp
+++ cpp01/ex05/src/Harl.cpp
@@ -2,30 +2,40 @@
 
 Harl::Harl(void){
 	std::cout << "Constructor called" << std::endl;
-	return;
+	return ;
 }
 
 Harl::~Harl(void){
 	std::cout << "Destructor called" << std::endl;
 }
 
-void	Harl::complain(std::string lvl){
-// left off here
+void	Harl::complain(std::string level){
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void		(Harl::*functions[4])(void) = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
+
+	for (int i = 0; i < 4; i++){
+		if (level == levels[i]){
+			(this->*functions[i])();
+			return ;
+		}
+	}
+	std::cerr << "Unknown level: " << level << std::endl;
+	return ;
 }
 
 void	Harl::_debug(void){
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
-	return;
+	return ;
 }
 
 void	Harl::_info(void){
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
-	return;
+	return ;
 }
 
 void	Harl::_warning(void){
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
-	return;
+	return ;
 }
 
 void	Harl::_error(void){
