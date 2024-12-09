d27c0fbee871501724210891b867478f244edbee
Michelle <m.stegeman@me.com>
2024-12-09 11:10:30 +0100

    improve main

--- cpp06/ex02/src/main.cpp
+++ cpp06/ex02/src/main.cpp
@@ -1,13 +1,21 @@
 #include "Base.hpp"
+#include <iostream>
+#include "colours.hpp"
 
 int main(void) {
 	Base*	base = new Base();
 	Base*	generated = base->generate();
 	Base*	generated2 = base->generate();
 
+	std::cout << UNDERLINE "Generated 1" NC << std::endl;
+	std::cout << "Identify by pointer:	";
 	base->identify(generated);
+	std::cout << "Identify by reference:	";
 	base->identify(*generated);
+	std::cout << UNDERLINE "\nGenerated 2" NC << std::endl;
+	std::cout << "Identify by pointer:	";
 	base->identify(generated2);
+	std::cout << "Identify by reference:	";
 	base->identify(*generated2);
 
 	delete generated;
